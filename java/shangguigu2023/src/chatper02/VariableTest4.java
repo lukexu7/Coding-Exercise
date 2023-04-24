111b96fd954fdf8e0333fc57addf5d7c9e8c61e6
Secant7 <2120982392@qq.com>
2023-04-24 14:32:46 +0800

    java review Variable Test

---
+++ java/shangguigu2023/src/chatper02/VariableTest4.java
@@ -0,0 +1,13 @@
+package chatper02;
+
+public class VariableTest4 {
+    public static void main(String[] args) {
+        double d1 = 12;
+        int i1 = (int) d1;
+
+        long l1 = 123;
+        short s2 = (short) l1;
+
+        syso
+    }
+}
