c169df3c58afbf67c7b0d3b02c81f7a8f6965835
LukeXu <2120982392@qq.com>
2024-04-13 21:12:30 +0800

    scala abstract class

---
+++ Scala/scala_tutorial/src/main/scala/chapter06/Scala11_Object_Abstract_1.scala
@@ -0,0 +1,38 @@
+package chapter06
+
+object Scala11_Object_Abstract {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+
+
+
+
+
+
+  }
+
+  abstract class User{
+
+    def test():Unit
+
+  }
+
+  class ChildUser extends User{
+
+    def test():Unit={
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+}
